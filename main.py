bf548284b0a87296e53ee004edc9b2fc6c6b9b41
TomEscobC <tomas.escobar@genstudents.org>
2024-11-26 15:35:56 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Comentario
+
+nombre = input("Ingresa tu nombre: ");
+        
+apellido = input("Ingresa tu apellido: ");
+
+ciudad = input("Ingresa tu ciudad: ");
+
+print(f"Hola, {nombre} {apellido}, seleccionaste la ciudad : {ciudad}.");
+
+nombre_completo = f"{nombre} {apellido}";
+
+print(f"nombre completo: {nombre_completo}");
